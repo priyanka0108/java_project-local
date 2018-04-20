b426a596df264b0990c3fae0b9f8e5107e4389ec
admin <gokulsk.m@gmail.com>
2018-04-20 06:26:43 +0530

    wap-1235 jenkins file

---
+++ python.py
@@ -0,0 +1,28 @@
+#echo "Python script for GIT..."
+
+items= ["1. check the git status","2. run git add","3. run git commit"]
+
+user=`whoami`
+
+ans=input("Enter your choise:")
+
+print("Entered value is:"$ans)
+
+"""
+
+$ans=$
+if ($ans == "1")
+{
+	git status
+}
+
+if ($ans == "2")
+{		
+git add .
+}
+if ($ans == "3")
+{
+git commit -m "files added by $user"
+}
+"""
+
