c2ecaafce75df36e3e8b7a5bc7da599302ee7ac7
admin <gokulsk.m@gmail.com>
2018-05-04 23:28:03 +0530

    bat file added

---
+++ run.bat
@@ -0,0 +1,6 @@
+echo "hello , testing post commit "
+echo "Dev2 user working..."
+echo "Dev2-New hooks changes"
+echo "commit-msg file updated for jira id in the message..test."
+echo "Test Report1"
+echo "Test Report2"
